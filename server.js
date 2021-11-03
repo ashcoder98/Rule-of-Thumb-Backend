bdbe7a836490ff4a0b71e2162338ad9ba88dba26
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2021-11-03 15:22:53 -0400

    add schema model

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const mongoose = require('mongoose');
 mongoose.connect(DATABASE_URL);
 
 const db = mongoose.connection
+db.on('open', ()=> console.log('You are connected to MongoDB'));
+db.on('close', ()=> console.log('You are disconnected to MongoDB'));
+db.on('error', (error) => console.log(error));
 
 
 app.get('/', (req, res) => {
